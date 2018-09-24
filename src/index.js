bb2c08524d080bde2505b771ef7d12ed5f91fc0a
vldmr1 <vldmr.one@gmail.com>
2018-09-24 22:36:35 +0300

    solution to quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let elementsOfEquation = equation.split(' ');
+  let a = (elementsOfEquation[0]);
+  let b = (elementsOfEquation[3] + elementsOfEquation[4]);
+  let c = (elementsOfEquation[7] + elementsOfEquation[8]);
+  let d = (b * b) - (4 * a * c);
+  
+  if (d === 0) {
+      let x1 = (b * -1) / (2 * a);
+      return x;
+  } else if (d > 0) {
+      let x1 = (-b + (Math.sqrt(d))) / (2 * a);
+      let x2 = (-b - (Math.sqrt(d))) / (2 * a);
+      let solution = [Math.round(x1), Math.round(x2)].sort((a, b) => a - b);
+      return solution;
+  } else {
+    return 'The equation has no real roots'
+  }
 }
+
